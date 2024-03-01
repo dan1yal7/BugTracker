691f0d49dc390c604bcc9a3d32ecbbc96a4d1723
dan1yal7 <daniiltemirkhanov@gmail.com>
2024-03-02 02:35:29 +0500

    RepositoryService

---
+++ Bug Tracker/BackroundServices/IBugRepositorycs.cs
@@ -0,0 +1,55 @@
+﻿using Bug_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bug_Tracker.BackroundServices
+{
+    public interface IBugRepositorycs
+    { 
+        Bug GetById(int id);    
+        IEnumerable<Bug> GetAllBugs();
+        void Add(Bug bug);  
+        void Update(Bug bug);   
+        void Delete(Bug bug);
+    }
+
+    public class BugRepository : IBugRepositorycs
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Bug> _dbSet;
+
+        public BugRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<Bug>();
+
+        } 
+
+        public Bug GetById(int id)
+        {
+            return _dbSet.Find(id); 
+        }
+
+        public IEnumerable<Bug> GetAllBugs()
+        {
+            return _dbSet.ToList(); 
+        } 
+
+        public void Add(Bug bug) 
+        { 
+            _dbSet.Add(bug);
+        
+        } 
+        public void Update(Bug bug)
+        {
+            _context.Entry(bug).State = EntityState.Modified;
+        }
+
+        public void Delete(Bug bug)  
+        {
+              _dbSet.Remove(bug);
+        
+        }
+
+    }
+    
+}
---
+++ Bug Tracker/Controllers/BugController.cs
@@ -0,0 +1,18 @@
+﻿using Bug_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bug_Tracker.Controllers
+{
+    public class BugController : Controller
+    {
+        private readonly ApplicationDbContext _context; 
+
+        public BugController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+      
+    }
+}
+ 
